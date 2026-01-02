1fca9b50f9a91c780e2d4c2d920964a82b83df2a
X <50459973+ly4096x@users.noreply.github.com>
2026-01-01 23:47:24 -0500

    Refactor user variable names in entrypoint script

--- usr/local/sbin/docker-entrypoint.sh
+++ usr/local/sbin/docker-entrypoint.sh
@@ -2,29 +2,29 @@
 
 set -eu
 
-UID=${DEFAULT_UID:-1000}
-GID=${DEFAULT_GID:-1000}
-USER=${DEFAULT_USERNAME:-gemini}
-HOME=${DEFAULT_HOME_DIR:-/home/$USER}
+USER_UID=${DEFAULT_UID:-1000}
+USER_GID=${DEFAULT_GID:-1000}
+USER_NAME=${DEFAULT_USERNAME:-gemini}
+USER_HOME=${DEFAULT_HOME_DIR:-/home/$USER_NAME}
 
 echo "Creating unprivileged user matching system user..."
-echo "  Name:     $USER"
-echo "  UID:      $UID"
-echo "  GID:      $GID"
-echo "  Home dir: $HOME"
+echo "  Name:     $USER_NAME"
+echo "  UID:      $USER_UID"
+echo "  GID:      $USER_GID"
+echo "  Home dir: $USER_HOME"
 
 # create group if it doesn't exist
-getent group $GID 2>&1 > /dev/null || addgroup --gid $GID $USER
+getent group $USER_GID 2>&1 > /dev/null || addgroup --gid $USER_GID $USER_NAME
 
 # create user if it doesn't exist
-getent passwd $UID 2>&1 > /dev/null || adduser --disabled-password --no-create-home --gecos "" --home "$HOME" --uid $UID --gid $GID $USER
+getent passwd $USER_UID 2>&1 > /dev/null || adduser --disabled-password --no-create-home --gecos "" --home "$USER_HOME" --uid $USER_UID --gid $USER_GID $USER_NAME
 
-chown $USER:$USER $HOME
-gosu $USER:$USER bash -c "mkdir -p $HOME/.gemini $HOME/.local/bin"
-export PATH="$HOME/.local/bin:$HOME/.local/sbin:$PATH"
+chown $USER_NAME:$USER_NAME $USER_HOME
+gosu $USER_NAME:$USER_NAME bash -c "mkdir -p $USER_HOME/.gemini $USER_HOME/.local/bin"
+export PATH="$USER_HOME/.local/bin:$USER_HOME/.local/sbin:$PATH"
 
 if [[ "$ENABLE_SUDO" -eq 1 ]]; then
     echo 'gemini ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/gemini
 fi
 
-gosu $USER:$USER "$@"
+gosu $USER_NAME:$USER_NAME "$@"
